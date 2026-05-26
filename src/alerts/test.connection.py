7964d2af9f7712c8168d2ef8124df95e918797c3
Ashwinm2001 <Ashwin2001hitech@gmail.com>
2026-05-26 17:27:29 +0530

    modified the test.connection.py

--- src/alerts/test.connection.py
+++ src/alerts/test.connection.py
@@ -2,7 +2,7 @@ from google.cloud import bigquery
 from google.oauth2 import service_account
 
 credentials = service_account.Credentials.from_service_account_file(
-    "Credential/retail-pulse-496303-ca384c444638.json"
+    "credentials/retail-pulse-496303-ca384c444638.json"
 )
 
 client = bigquery.Client(
